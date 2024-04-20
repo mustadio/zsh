1321b0415c28f6d68ffbb2158d179e6238fb643f
qian.shen <csshenqian@gmail.com>
2024-04-20 23:11:17 +0800

    First commit

---
+++ init.zsh
@@ -0,0 +1,12 @@
+source ~/.config/zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+
+skip_global_compinit=1
+
+
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+zstyle ':vcs_info:git:*' formats '%b '
+
+setopt PROMPT_SUBST
+PROMPT='%F{green}%*%f %F{cyan}%m%f %F{blue}%~%f %F{magenta}${vcs_info_msg_0_}%f$ '
