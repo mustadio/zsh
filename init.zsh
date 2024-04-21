0a40b4b34372170599e88595e1e7bfb7cf5611a5
qian.shen <csshenqian@gmail.com>
2024-04-21 17:47:05 +0800

    shorten hostname

--- init.zsh
+++ init.zsh
@@ -6,10 +6,25 @@ HISTFILE="$HOME/.zsh_history"
 HISTSIZE=100000
 SAVEHIST=100000
 
+setopt APPEND_HISTORY
+
+
 autoload -Uz vcs_info
 precmd() { vcs_info }
 
 zstyle ':vcs_info:git:*' formats '%b '
 
+if [[ $HOST == "Chester-Desktop" ]]
+then
+  SHORTHOST="MyPC"
+elif [[ $HOST == "chestsh-server" ]]
+then
+  SHORTHOST="GateWay"
+else
+  SHORTHOST=%m
+fi
+
 setopt PROMPT_SUBST
-PROMPT='%F{green}%*%f %F{cyan}%m%f %F{blue}%1~%f %F{magenta}${vcs_info_msg_0_}%f$ '
+PROMPT='$SHORTHOST %F{blue}%2~%f %F{magenta}${vcs_info_msg_0_}%f$ '
+
+
